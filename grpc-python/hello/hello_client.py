6965aeb0fb534fffa4d34c10bff0495249e45440
Yihui Zhao <yzhao@redhat.com>
2018-07-03 11:32:29 +0800

    update the tools and code

---
+++ grpc-python/hello/hello_client.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+import grpc
+
+import hello_pb2
+import hello_pb2_grpc
+
+def run():
+    channel = grpc.insecure_channel("localhost:50051")
+    stub = hello_pb2_grpc.ActionStub(channel)
+
+    response = stub.hihi(hello_pb2.ActionRequest(action='zyh'))
+    print ("Greeter client received: " + response.message)
+
+if __name__ == '__main__':
+    run()
