b579ddc95fb572e9796adc4995a65953883f23c4
Yihui Zhao <yzhao@redhat.com>
2018-07-07 19:50:00 +0800

    remove some files

--- RPC/client_test_copy.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env/python3
-
-__author__ = "kenzhaoyihui"
-
-from xmlrpclib import ServerProxy
-
-s = ServerProxy("http://127.0.0.1:8000")
-s.hello()
--- grpc-python/route_guide/run_codegen.py
+++
@@ -1,9 +0,0 @@
-from grpc_tools import protoc
-
-protoc.main((
-    '',
-    '-I  ../protos',
-    '--python_out=../route_guide/',
-    '--grpc_python_out=../route_guide/',
-    '../protos/route_guide.proto',
-))
